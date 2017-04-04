a85e333beec74e598857312c5206944b7a77cc69
BowenLiu <bliu3535@gmail.com>
2017-04-04 14:47:25 -0700

    add about paragraph

--- circle-lu/client/src/about/components/About.js
+++ circle-lu/client/src/about/components/About.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 
 class About extends Component {
-  render () {
+  render() {
     return (
       <div>
-        Hello
+        <h2>Bio</h2>
+        <p>Chinese b. 1991.</p>
+        <p>Yuan (Circle) Lu is a Chinese fine-art photographer now based in Manhattan, New York. His conceptual art
+          practices always attempt to express simple ideas through subtleties by either using digital techniques or
+          elaborately choosing subjects, while at the same time, pursuing unique beauty as well.</p>
+        <p>He also has great interest in design field. He would like to combine his techniques with any form of creative
+          visual products, and is now looking for opportunities to work in a photography studio or creative studio, to
+          further establish his working experience.</p>
+        <p>School of Visual Arts, Digital Photography, MPS</p>
+        <p>Fordham University School of Law, Banking Corporate & Finace Law, LLM</p>
+        <h3>Contact</h3>
+        <p>Email: info@circleluyuan.com</p>
+        <p>Instagram: @circleluyuan</p>
+        <p>Résumé is available upon request.</p>
       </div>
     );
   }
